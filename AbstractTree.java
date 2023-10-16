4cf9ffc369ba6956569e901e840b8b5b02871cc3
ThatchanonPoommanee <148045739+ThatchanonPoommanee@users.noreply.github.com>
2023-10-16 16:52:23 +0700

    Add files via upload

---
+++ AbstractTree.java
@@ -0,0 +1,50 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
+
+/** An abstract base class providing some functionality of the Tree interface. */
+public abstract class AbstractTree<E> implements Tree<E> {
+	@Override
+	public boolean isInternal(Position<E> p) { 
+		return numChildren(p) > 0; 
+		}
+	
+	@Override
+	public boolean isExternal(Position<E> p) { 
+		return numChildren(p) == 0; 
+		}
+	
+	@Override
+	public boolean isRoot(Position<E> p) { 
+		return p == root( ); 
+		}
+	
+	@Override
+	public boolean isEmpty( ) { 
+		return size( ) == 0; 
+		}
+	
+	public int depth(Position<E> p) {
+		if(isRoot(p))
+			return 0;
+		else
+			return 1 + depth(parent(p));
+	}
+	
+	private int heightBad() {
+		int h = 0;
+		for (Position<E> p: positions())
+			if(isExternal(p))
+				h = Math.max(h, depth(p));
+		return h;
+	}
+	
+	/** Returns the height of the subtree rooted at Position p. */
+	public int height(Position<E> p) {
+		int h = 0; // base case if p is external
+		for (Position<E> c : children(p))
+		h = Math.max(h, 1 + height(c));
+		return h;
+		}
+}
+	
